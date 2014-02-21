f2ec98ae8fbaa08dbb64a7758dfb9af03fcbd6fd
Yuki Koyama <koyama@is.s.u-tokyo.ac.jp>
2014-02-21 21:55:43 +0900

    Describe necessary APIs for Interpolator class

--- RBFInterpolator/interpolator.cpp
+++ RBFInterpolator/interpolator.cpp
@@ -1,5 +1,33 @@
 #include "interpolator.h"
 
-Interpolator::Interpolator()
+using namespace std;
+
+Interpolator::Interpolator() :
+    functionType(GAUSSIAN),
+    epsilon(10.0),
+    readyForUse(false)
 {
+}
+
+void Interpolator::addCenterPoint(double y, vector<double> x)
+{
+    // todo
+
+    readyForUse = false;
+}
+
+void Interpolator::computeWeights()
+{
+    // todo
+
+    readyForUse = true;
+}
+
+double Interpolator::getInterpolatedValue(vector<double> x)
+{
+    if (!readyForUse) {
+        return 0.0;
+    }
+
+    // todo
 }
--- RBFInterpolator/interpolator.h
+++ RBFInterpolator/interpolator.h
@@ -1,10 +1,36 @@
 #ifndef INTERPOLATOR_H
 #define INTERPOLATOR_H
 
+#include <vector>
+
 class Interpolator
 {
 public:
     Interpolator();
+
+    // function definition
+    enum FUNCTION_TYPE {
+        GAUSSIAN,       // f(r) = exp(-(epsilon*r)^2)
+    } functionType;
+
+    double epsilon;
+
+    // API
+    void    addCenterPoint(double y, std::vector<double> x);
+    void    computeWeights();
+    double  getInterpolatedValue(std::vector<double> x);
+
+private:
+
+    // registered center points
+    std::vector<double>               ys;
+    std::vector<std::vector<double> > xs;
+
+    // computed weights
+    std::vector<double>               w;
+
+    // for error check
+    bool readyForUse;
 };
 
 #endif // INTERPOLATOR_H
